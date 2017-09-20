f3cdc35a91cb708a4c020a0e43b02f23621d1abb
chris30269 <cernst3@gatech.edu>
2017-09-20 15:15:08 -0400

    Create exrx.js

---
+++ exrx.js
@@ -0,0 +1,21 @@
+var exrx = {
+	"link":"http://www.exrx.net/WeightExercises/Sternocleidomastoid/CBNeckFlx.html",
+	"name":"Cable Neck Flexion",
+	"body part":"Neck",
+	"target":"Sternocleidomastoid",
+	"synergists":"",
+	"stabilizers":["Rectus Abdominis", "Obliques"],
+	"equipment":1,
+	"mechanics":"Isolated",
+	"force":"pull",
+	"gif":"http://www.exrx.net/AnimatedEx/Sternocleidomastoid/CBNeckFlexion.gif",
+	"video":"https://i.vimeocdn.com/video/559267730.webp",
+	"vimeo":"559267730"
+};
+
+var equipment = {
+	"1":"Cable Machine",
+	"2":"Dumbbell",
+	"3":"Barbell",
+	"4":"EZ Bar"
+}
