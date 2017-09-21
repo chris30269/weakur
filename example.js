354e64b23fef805e855cef142d85783638e0a3b1
chris30269 <cernst3@gatech.edu>
2017-09-21 13:00:35 -0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,61 @@
+var workout = {
+	"startTime":1506008178,
+	"endTime":1506011178,
+	"movements":[{
+		"id":1,
+		"units":1,
+		"order":1
+		"sets":[{
+			"reps":10,
+			"startTime":1506008198,
+			"endTime":1506008228,
+			"notes":"palms up",
+			"rest":60,
+			"weight":35
+		},
+		{
+			"reps":10,
+			"startTime":1506008278,
+			"endTime":1506008288,
+			"notes":"palms down",
+			"rest":60,
+			"weight":35
+		},
+		{
+			"reps":9,
+			"startTime":1506008358,
+			"endTime":1506008378,
+			"notes":"palms neutral",
+			"rest":60,
+			"weight":35
+		}
+			],
+		"notes":"go up next time",
+		"superset":true
+	}]
+};
+
+var log = {
+	"time":1506008978,
+	"movements":[
+		{
+			"id":1,
+			"units":1,
+			"weight":100,
+			"reps":1
+		},
+		{
+			"id":2,
+			"units":1,
+			"weight":10,
+			"reps":1
+		}
+	],
+	"measurements":[
+		{
+			"part":1,
+			"measure":12,
+			"units":4
+		}
+	]
+};
