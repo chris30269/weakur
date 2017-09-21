67fdb7bfebd20681b8e3c0906d7b692429cbdf4e
Pannycakes <tsayeau@gmail.com>
2017-09-21 13:32:51 -0400

    Update exrx.js

--- exrx.js
+++ exrx.js
@@ -15,4 +15,21 @@ var exrx = {
 		"video":"https://i.vimeocdn.com/video/559267730.webp",
 		"vimeo":"559267730",
 	}
+	
+	"2":{
+		"id":2,
+		"link":"http://www.exrx.net/WeightExercises/TrapeziusUpper/BWInvertedShrugPB.html",
+		"name":"Inverted Shrug (on parallel bars)",
+		"body part":"Traps",
+		"target":"Trapezius",
+		"synergists":"Levator Scapulae",
+		"stabilizers":"",
+		"equipment":1,
+		"goal":1,
+		"mechanics":"Isolated",
+		"force":"pull",
+		"gif":"http://www.exrx.net/AnimatedEx/TrapeziusUpper/BWBentKneeInvertedShrug.gif",
+		"video":"https://i.vimeocdn.com/video/559281548.webp",
+		"vimeo":"559281548",
+	}
 };
