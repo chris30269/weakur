ebb419c08172d03cf61c967684c05e160378618e
chris30269 <cernst3@gatech.edu>
2017-09-21 13:01:02 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,43 @@
+var units = {
+	1:"lbs",
+	2:"kg",
+	3:"bodyweight",
+	4:"in",
+	5:"cm",
+	6:"sec",
+	7:"min",
+	8:"ft",
+	9:"m",
+	10:"mi",
+	11:"km"
+}
+
+var muscles = [
+	{
+		"id":1,
+		"sciName":"Sternocleidomastoid",
+		"part":1
+	}
+];
+
+var bodyParts = [
+	{
+		"id":1,
+		"name":"Neck"
+	}
+];
+
+var equipment = {
+	"1":"Cable Machine",
+	"2":"Dumbbell",
+	"3":"Barbell",
+	"4":"EZ Bar"
+};
+
+var goals = {
+	"1":"sets and reps",
+	"2":"reps",
+	"3":"max time",
+	"4":"min time",
+	"5":"distance"
+};
