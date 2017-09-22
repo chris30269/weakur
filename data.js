14cdef6f8c84afabe8ca469edb60e2b597b4a102
Pannycakes <tsayeau@gmail.com>
2017-09-21 21:07:19 -0400

    Update data.js
    added additional body parts as well as more muscles

--- data.js
+++ data.js
@@ -12,32 +12,74 @@ var units = {
 	11:"km"
 }
 
-var muscles = [
-	{
+var muscles = {
+	1:{
 		"id":1,
 		"sciName":"Sternocleidomastoid",
 		"part":1
+	},
+	2:{
+		"id":2,
+		"sciName":"Rectus Abdominis",
+		"part":2
+	},
+	3:{
+		"id":3,
+		"sciName":"Obliques",
+		"part":2
 	}
-];
+	4:{
+		"id":4,
+		"sciName":"Trapezius",
+		"part":1
+	}
+	5:{
+		"id":5,
+		"sciName":"Levator Scapulae",
+		"part":3
+	}
+};
 
-var bodyParts = [
-	{
-		"id":1,
-		"name":"Neck"
-	}
-];
+var bodyParts = {
+	1:"Neck",
+	2:"Abs",
+	3:"Back",
+	4:"Arms",
+	5:"Legs",
+	6:"Glutes",
+	7:"Calves",
+	8:"Pelvic Floor",
+	9:"Chest",
+};
+
+var userPrefs = {
+	"units":1,
+	"reps":8,
+	"sets":3,
+	"rest":60
+};
 
 var equipment = {
-	"1":"Cable Machine",
-	"2":"Dumbbell",
-	"3":"Barbell",
-	"4":"EZ Bar"
+	1:"Cable Machine",
+	2:"Dumbbell",
+	3:"Barbell",
+	4:"EZ Bar"
 };
 
 var goals = {
-	"1":"sets and reps",
-	"2":"reps",
-	"3":"max time",
-	"4":"min time",
-	"5":"distance"
+	1:"sets and reps",
+	2:"reps",
+	3:"max time",
+	4:"min time",
+	5:"distance"
 };
+
+var mechanics = {
+	1:"Isolated",
+	2:"Compound"
+}
+
+var force = {
+	1:"Pull",
+	2:"Push"
+}
